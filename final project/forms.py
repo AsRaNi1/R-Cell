6a351d0d17a2d1f4ee39481f7875eaaa2517f292
Arnav Zutshi <84843295+AsRaNi1@users.noreply.github.com>
2022-01-20 01:12:29 +0530

    arnav

---
+++ final project/forms.py
@@ -0,0 +1,25 @@
+from wtforms import Form, StringField, DecimalField, IntegerField, TextAreaField, PasswordField, validators
+
+# form used on Register page
+
+
+class RegisterForm(Form):
+    name = StringField('Full Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [validators.DataRequired(
+    ), validators.EqualTo('confirm', message='Passwords do not match')])
+    confirm = PasswordField('Confirm Password')
+
+# form used on the Transactions page
+
+
+class SendMoneyForm(Form):
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    amount = StringField('Amount', [validators.Length(min=1, max=5)])
+
+# form used on the Buy page
+
+
+class BuyForm(Form):
+    amount = StringField('Amount', [validators.Length(min=1, max=1)])
